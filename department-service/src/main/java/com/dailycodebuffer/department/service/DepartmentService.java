694a0b2ecb9d52861c293745b102c59803c51865
Hedsw <yun.hyeok.lee@gmail.com>
2021-02-26 16:08:49 -0500

    department Part

---
+++ department-service/src/main/java/com/dailycodebuffer/department/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.dailycodebuffer.department.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.dailycodebuffer.department.entity.Department;
+import com.dailycodebuffer.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside SaveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside SaveDepartment of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+}
